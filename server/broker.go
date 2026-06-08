b842a94d4fd5a0608961d0a402dfdf5c081d846e
nxkh4ng <kh4ngnx@gmail.com>
2026-06-08 16:42:16 +0700

    feat(server): add SSE broker for managing browser clients

---
+++ server/broker.go
@@ -0,0 +1,55 @@
+package main
+
+import "sync"
+
+type Client struct {
+	Events chan string
+}
+
+func NewClient() *Client {
+	return &Client{
+		Events: make(chan string, 64),
+	}
+}
+
+type Broker struct {
+	mu      sync.Mutex
+	clients map[*Client]struct{}
+}
+
+func NewBroker() *Broker {
+	return &Broker{
+		clients: make(map[*Client]struct{}),
+	}
+}
+
+func (b *Broker) Add() *Client {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	client := NewClient()
+	b.clients[client] = struct{}{}
+	return client
+}
+
+func (b *Broker) Remove(client *Client) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	delete(b.clients, client)
+	close(client.Events)
+}
+
+func (b *Broker) Broadcast(event string) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	for client := range b.clients {
+		select {
+		case client.Events <- event:
+			// send success
+		default:
+			// client slow, skip (not block)
+		}
+	}
+}
